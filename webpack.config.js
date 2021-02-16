b6754e432dc55363fd1c481b1de05a98989fd2d6
paleika <nadzeya.paleika@gmail.com>
2021-02-16 10:38:29 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    mode: "development",
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: [
+                    { 
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+}
