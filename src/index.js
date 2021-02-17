798b6c84d050bc595d4bbe010646f875b0a46f28
paleika <nadzeya.paleika@gmail.com>
2021-02-17 16:16:32 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import puppeteer from 'puppeteer'
 
 const App = () => <p>Hello World</p>
 
 ReactDOM.render(<App />,
     document.getElementById('root'));
+
+// (async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://www.google.com');
+//     await page.screenshot({ path: 'google.png' });
+//     await browser.close()
+// })();
