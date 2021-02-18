fbc514ee8f5b8c68abf5b9a9d732ce503712cab4
paleika <nadzeya.paleika@gmail.com>
2021-02-18 14:05:03 +0300

    the watch mode trouble

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
-    headless: process.env.HEADLESS === 'false'
-  }
+    devtools: process.env.HEADLESS === 'false'
+  },
+  server: {
+    command: 'npm start',
+    port: 8080,
+  },
 }
